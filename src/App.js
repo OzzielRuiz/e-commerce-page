5442ff03676808273b641ae6c37d74a6d8de6901
unknown <ozziel-isaias.ruiz-de-regules.1@ens.etsmtl.ca>
2020-08-24 20:47:00 -0400

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import Homepage from './pages/homepage/homepage-component'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage-component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats PAge</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div >
-      <Homepage />
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
+      </Router>
+      {/* <Homepage /> */}
     </div>
   );
 }
